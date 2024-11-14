c732062cb4ab4684d69c8a75b909356fc00e9faa
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-11-14 06:16:19 -0800

    blog

--- components/organisms/BlogSearch.jsx
+++ components/organisms/BlogSearch.jsx
@@ -41,11 +41,11 @@ const BlogSearch = () => {
       <div className='text-center max-w-xl mx-auto'>
         <Heading level={3}>Our Blogs</Heading>
         <div className="text-center mb-4">
-          <span className='inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1'></span>
-          <span className='inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1'></span>
-          <span className='inline-block w-40 h-1 rounded-full bg-indigo-500 ml-1'></span>
-          <span className='inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1'></span>
-          <span className='inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1'></span>
+          <span className='inline-block w-1 h-1 rounded-full bg-[#05C5FA] ml-1'></span>
+          <span className='inline-block w-3 h-1 rounded-full bg-[#05C5FA] ml-1'></span>
+          <span className='inline-block w-40 h-1 rounded-full bg-[#05C5FA] ml-1'></span>
+          <span className='inline-block w-3 h-1 rounded-full bg-[#05C5FA] ml-1'></span>
+          <span className='inline-block w-1 h-1 rounded-full bg-[#05C5FA] ml-1'></span>
         </div>
       </div>
 
